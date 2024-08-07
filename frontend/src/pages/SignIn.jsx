a40757d0302c2d28ecbbe5cfd19d25b266687842
Nirmal Dhakal <80183902+n3rm4121@users.noreply.github.com>
2024-08-07 23:24:21 +0545

    Update SignIn.jsx

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -4,6 +4,8 @@ import { login, signUp } from '../features/UserSlice';
 import { useNavigate } from 'react-router-dom';
 import ErrorDialog from '../components/ErrorDialog';
 import SuccessDialog from '../components/SuccessDialog';
+import GoogleSignIn from '../components/GoogleSignIn';
+import Spinner from '../components/Spinner';
 
 function SignIn() {
   const [isLogin, setIsLogin] = useState(true);
@@ -27,7 +29,7 @@ function SignIn() {
 
   useEffect(() => {
     if (error) {
-      setErrorMessage(error.message );
+      setErrorMessage(error.message);
       setTimeout(() => {
         setErrorMessage('');
       }, 2000);
@@ -46,14 +48,14 @@ function SignIn() {
       dispatch(login({ email, password })).then((result) => {
         setEmail('');
         setPassword('');
-        if (result.type.includes('fulfilled')) {        
+        if (result.type.includes('fulfilled')) {
           setSuccessMessage('Login successful!');
           setTimeout(() => {
             setSuccessMessage('');
           }, 2000);
         }
 
-        
+
       });
     } else {
       dispatch(signUp({ fullname, email, username, password })).then((result) => {
@@ -75,12 +77,12 @@ function SignIn() {
   return (
     <div className="min-h-screen flex flex-col md:flex-row  bg-gray-200 px-8 md:px-20">
       <div className="flex flex-col items-center justify-center lg:ml-36 w-full p-4 md:p-6 md:w-1/2">
-      <h1 className="text-6xl font-bold mb-4 text-primary">VidShare</h1>
+        <h1 className="text-6xl font-bold mb-4 text-primary">VidShare</h1>
         <p className="text-xl text-black">Share your favorite videos with the world.</p>
       </div>
       <div className="flex flex-col justify-center items-center w-full p-4 md:p-6 md:w-1/2 lg:mr-36">
-      <div className="bg-white p-6 rounded-xl shadow-lg w-full max-w-md">
-      <h1 className="text-2xl font-bold mb-6 text-center">{isLogin ? 'Login' : 'Signup'}</h1>
+        <div className="bg-white p-6 rounded-xl shadow-lg w-full max-w-md">
+          <h1 className="text-2xl font-bold mb-6 text-center">{isLogin ? 'Login' : 'Signup'}</h1>
           <form onSubmit={handleSubmit} className="space-y-4">
             {!isLogin && (
               <div>
@@ -131,6 +133,7 @@ function SignIn() {
                 required
               />
             </div>
+
             {!isLogin && (
               <div>
                 <label className="block text-sm">Confirm Password</label>
@@ -144,9 +147,14 @@ function SignIn() {
                 />
               </div>
             )}
-            <button type="submit" disabled={loading} className="w-full bg-primary text-white py-2 rounded">
-              {isLogin ? 'Login' : 'Signup'}
+            
+            <button type="submit" disabled={loading} className={`w-full bg-primary text-white py-2 rounded ${loading ? 'opacity-50 pointer-events-none': ''}`}>
+             
+              {loading ? <Spinner loading={loading} size={20} /> : isLogin ? 'Login' : 'Signup'}
             </button>
+
+
+
           </form>
           <div className="mt-4 text-center">
             {isLogin ? (
@@ -163,13 +171,22 @@ function SignIn() {
                   Log in
                 </button>
               </p>
+
             )}
+            <div className="mt-4 flex items-center justify-center">
+              <hr className='w-1/2' />
+              <p className="mx-2">OR</p>
+              <hr className="w-1/2" />
+            </div>
+
+            <GoogleSignIn />
+
           </div>
         </div>
         {errorMessage && <ErrorDialog message={errorMessage} onClose={() => setErrorMessage('')} />}
-        
-        
-        {successMessage && <SuccessDialog message={successMessage} onClose={() => setSuccessMessage('')} /> }
+
+
+        {successMessage && <SuccessDialog message={successMessage} onClose={() => setSuccessMessage('')} />}
       </div>
     </div>
   );
