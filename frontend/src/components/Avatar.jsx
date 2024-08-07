2a6f288e5674d91e8655d9d9e4d2ee8dce544d33
Nirmal Dhakal <80183902+n3rm4121@users.noreply.github.com>
2024-08-07 23:21:36 +0545

    Update Avatar.jsx

--- frontend/src/components/Avatar.jsx
+++ frontend/src/components/Avatar.jsx
@@ -13,7 +13,7 @@ function Avatar({ user , type}) {
     );
   } else {
     return (
-      <div className={`flex items-center justify-center bg-gray-300 text-gray-700 font-bold rounded-full p-2 ${type==='medium' && 'h-10 w-10 text-lg'} ${type ==='large' && 'w-[150px] h-[150px] text-7xl' } ${type==='small' && 'h-5 w-5 text-xs'} `
+      <div className={`flex items-center justify-center bg-gray-300 text-gray-700 font-bold rounded-full p-2 ${type==='medium' && 'h-8 w-8 text-lg'} ${type ==='large' && 'w-[150px] h-[150px] text-7xl' } ${type==='small' && 'h-5 w-5 text-xs'} `
 
       }>
         {user?.fullname?.charAt(0).toUpperCase()}
