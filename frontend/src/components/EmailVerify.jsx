cc26f3adf72a5dc1a0f8c739fb0dfe644702dd1a
Nirmal Dhakal <80183902+n3rm4121@users.noreply.github.com>
2024-08-07 23:22:02 +0545

    Update EmailVerify.jsx

--- frontend/src/components/EmailVerify.jsx
+++ frontend/src/components/EmailVerify.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
-import { baseURL } from '../utils/axiosInstance';
-
+const baseURL = import.meta.env.VITE_REACT_APP_BASE_URL;
 
 const VerifyEmail = () => {
     const { userId, token } = useParams();
