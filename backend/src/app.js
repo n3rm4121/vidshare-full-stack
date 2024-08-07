6c2aac92a8f14b94792792304561a448eb891056
Nirmal Dhakal <80183902+n3rm4121@users.noreply.github.com>
2024-08-07 23:36:01 +0545

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,9 +16,6 @@ app.use(express.urlencoded({extended: true, limit: '10kb'}))
 
 app.use(express.static("public"))
 
-
-
-
 //routes import
 import userRouter from './routes/user.routes.js'
 import videoRouter from './routes/video.routes.js'
@@ -27,6 +24,7 @@ import commentRouter from './routes/comment.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
 import likeRouter from './routes/like.routes.js'
 import notificationRouter from './routes/notification.routes.js'
+import authRouter from './routes/auth.routes.js'
 
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/videos", videoRouter)
@@ -35,5 +33,5 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/subscription", subscriptionRouter)
 app.use('/api/v1/likes', likeRouter);
 app.use('/api/v1/notifications', notificationRouter);
-
+app.use('/api/v1/auth', authRouter);
 export {app}
